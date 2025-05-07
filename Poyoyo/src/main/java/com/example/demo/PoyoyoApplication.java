374927715927f2305df79e53f8c657c0e2e8921d
vaytran <vaytran3910@gmail.com>
2025-05-07 15:35:49 +0700

    first commit

---
+++ Poyoyo/src/main/java/com/example/demo/PoyoyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoyoyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoyoyoApplication.class, args);
+	}
+
+}
